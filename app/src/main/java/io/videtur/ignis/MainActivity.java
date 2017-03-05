0fe6475451f53e333ebb14ac17a592dadef4018e
Eoin O'Brien <eoinobrien910@gmail.com>
2017-03-05 20:59:12 +0000

    Add user presence system to track connections and last online time

--- app/src/main/java/io/videtur/ignis/MainActivity.java
+++ app/src/main/java/io/videtur/ignis/MainActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -49,8 +48,7 @@ public class MainActivity extends IgnisAuthActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // TODO start NewMessageActivity
             }
         });
 
@@ -83,6 +81,7 @@ public class MainActivity extends IgnisAuthActivity
 
     @Override
     public void onUserDataChange(User user) {
+        super.onUserDataChange(user);
         Log.d(TAG, "user.getName:" + user.getName());
         Log.d(TAG, "user.getEmail:" + user.getEmail());
         Log.d(TAG, "user.getPhotoUrl:" + user.getPhotoUrl());
--- app/src/main/java/io/videtur/ignis/util/Constants.java
+++ app/src/main/java/io/videtur/ignis/util/Constants.java
@@ -3,7 +3,12 @@ package io.videtur.ignis.util;
 public final class Constants {
 
     // Firebase database paths
+    public static final String CONNECTED_REF = ".info/connected";
     public static final String USERS_REF = "users";
+
+    // Firebase database children
+    public static final String CONNECTIONS_CHILD = "connections";
+    public static final String LAST_ONLINE_CHILD = "lastOnline";
 
     private Constants() {
     }
