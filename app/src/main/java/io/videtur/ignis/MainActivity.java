c7e89844df3647d4b73ebf8e94d7097bbbdb8a89
Eoin O'Brien <eoinobrien910@gmail.com>
2017-03-05 01:11:52 +0000

    Change navigation drawer icons and remove main menu

--- app/src/main/java/io/videtur/ignis/MainActivity.java
+++ app/src/main/java/io/videtur/ignis/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -17,6 +16,8 @@ import io.videtur.ignis.util.IgnisAuthActivity;
 
 public class MainActivity extends IgnisAuthActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    // TODO Set navigation drawer icon, name and email address
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,28 +53,6 @@ public class MainActivity extends IgnisAuthActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
