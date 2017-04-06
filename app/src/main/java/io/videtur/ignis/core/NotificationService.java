35851ab44efb45abbb04f4d43726f69cf7cf5306
Eoin O'Brien <eoinobrien910@gmail.com>
2017-04-06 14:13:22 +0100

    Change notification icon to chat icon

--- app/src/main/java/io/videtur/ignis/core/NotificationService.java
+++ app/src/main/java/io/videtur/ignis/core/NotificationService.java
@@ -140,7 +140,7 @@ public class NotificationService extends Service {
                     .setContentIntent(pendingIntent)
                     .setContentTitle(getResources().getString(R.string.app_name))
                     .setContentText(getNotificationText(chatCount, totalMessageCount))
-                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setSmallIcon(R.drawable.ic_message)
                     .setLights(LED_COLOR, 1000, 1000);
             if (updates.size() > 0) {
                 mNotifyBuilder.setDefaults(NotificationCompat.DEFAULT_SOUND);
